3d5c8cb2c77596a455973b9472824fcef5cd1765
[dyad] <nassimhammoudi22@gmail.com>
2026-01-05 23:01:23 +0100

    Reverted all changes back to version 1312cd922c054a036be95671a7b38b9ea1648b00

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -4,9 +4,6 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Layout from '@/components/Layout';
 import LanguageSwitcher from '@/components/LanguageSwitcher';
-import { Link } from 'react-router-dom';
-import { Button } from '@/components/ui/button';
-import { ListFilter } from 'lucide-react';
 
 const SettingsPage = () => {
   const { t } = useTranslation();
@@ -21,17 +18,6 @@ const SettingsPage = () => {
           <div className="space-y-2">
             <h3 className="text-lg font-semibold">{t('language')}</h3>
             <LanguageSwitcher />
-          </div>
-
-          <div className="space-y-2">
-            <h3 className="text-lg font-semibold">{t('data_management')}</h3>
-            <Button asChild variant="outline" className="w-full justify-start">
-              <Link to="/settings/categories">
-                <ListFilter className="mr-2 h-4 w-4" />
-                {t('category_management')}
-              </Link>
-            </Button>
-            {/* Add more data management options here */}
           </div>
           {/* Add more settings options here */}
         </div>
